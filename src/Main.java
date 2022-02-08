8371b36c62f2654e82d313c36e25e6838f14964c
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-02-07 21:20:31 -0600

    changes in main
    cambié el main para que corra con una función switch

--- src/Main.java
+++ src/Main.java
@@ -15,28 +15,32 @@ public class Main {
             }
             System.out.println("Seleccione tipo de sort: \n1. GnomeSort \n2. Merge sort \n3. Quick sort \n4. Radix sort \n5. Bubble sort");
             int op = scan.nextInt();
-            if (op ==1){
-                int arr1[] = new int[numero];
-                GnomeSort gs = new GnomeSort();
-                gs.gnomeSort(arr, arr.length);
-
-            } if (op == 2){
-                mergeSort ms = new mergeSort();
-                ms.sort(arr, 0, arr.length -1);
-            } if (op == 3){
-                QuickSort qs = new QuickSort();
-                qs.quickSort(arr, 0, arr.length-1);
-            } if (op == 4){
-                RadixSort rs = new RadixSort();
-                rs.radixsort(arr, arr.length);
-            } if (op == 5){
-                BubbleSort bs = new BubbleSort();
-                bs.bubbleSort(arr);
-            } else {
-                System.out.println("Ingrese de  1 a 5");
+            switch (op) {
+                case 1:
+                    int arr1[] = new int[numero];
+                    GnomeSort gs = new GnomeSort();
+                    gs.gnomeSort(arr, arr.length);
+                    break;
+                case 2:
+                    mergeSort ms = new mergeSort();
+                    ms.sort(arr, 0, arr.length - 1);
+                    break;
+                case 3:
+                    QuickSort qs = new QuickSort();
+                    qs.quickSort(arr, 0, arr.length - 1);
+                    break;
+                case 4:
+                    RadixSort rs = new RadixSort();
+                    rs.radixsort(arr, arr.length);
+                    break;
+                case 5:
+                    BubbleSort bs = new BubbleSort();
+                    bs.bubbleSort(arr);
+                    break;
+                default:
+                    System.out.println("Ingrese de  1 a 5");
+                    break;
             }
         }
-
-
     }
 }
