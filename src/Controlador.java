77c1111d0cdcccf109a2dee0834e62ed98c04662
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-02-09 18:58:08 -0600

    cambios en controlador
    se agregó una función para verificar que la entrada sea de un número y se cambió el main de acuerdo a esta función.

--- src/Controlador.java
+++ src/Controlador.java
@@ -8,4 +8,16 @@ public class Controlador {
             System.out.println(array[i]);
         }
     }
+    public boolean verificarInt(String entrada)
+    {
+        try{
+            int i = Integer.parseInt(entrada);
+            return true;
+        }catch(Exception e)
+        {
+            System.out.println("Ingrese una opcion valida");
+            return false;
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,40 +11,50 @@ public class Main {
         Random rand = new Random();
         boolean on = true;
         while(on){
-            System.out.println("Ingrese cantidad de numeros a sortear");
-            int numero = scan.nextInt();
+            int numero = 0;
+            boolean entradaIncorrecta = true;
+            while(entradaIncorrecta)
+            {
+                System.out.println("Ingrese cantidad de numeros a sortear");
+                String eleccion = scan.nextLine();
+                if(contro.verificarInt(eleccion))
+                {
+                    numero = Integer.parseInt(eleccion);
+                    entradaIncorrecta = false;
+                }
+            }
             int arr[] = new int[numero];
             for (int i = 0; i < numero; i++) {
                 arr[i] = abs(rand.nextInt());
             }
             System.out.println("Seleccione tipo de sort: \n1. GnomeSort \n2. Merge sort \n3. Quick sort \n4. Radix sort \n5. Bubble sort\n Si desea salir ingrese 0");
-            int op = scan.nextInt();
+            String op = scan.nextLine();
             switch (op) {
-                case 0:
+                case "0":
                     System.exit(0);
                     break;
-                case 1:
+                case "1":
                     int arr1[] = new int[numero];
                     GnomeSort gs = new GnomeSort();
                     gs.gnomeSort(arr, arr.length);
                     contro.imprimirArray(arr);
                     break;
-                case 2:
+                case "2":
                     mergeSort ms = new mergeSort();
                     ms.sort(arr, 0, arr.length - 1);
                     contro.imprimirArray(arr);
                     break;
-                case 3:
+                case "3":
                     QuickSort qs = new QuickSort();
                     qs.quickSort(arr, 0, arr.length - 1);
                     contro.imprimirArray(arr);
                     break;
-                case 4:
+                case "4":
                     RadixSort rs = new RadixSort();
                     rs.radixsort(arr, arr.length);
                     contro.imprimirArray(arr);
                     break;
-                case 5:
+                case "5":
                     BubbleSort bs = new BubbleSort();
                     bs.bubbleSort(arr);
                     contro.imprimirArray(arr);
